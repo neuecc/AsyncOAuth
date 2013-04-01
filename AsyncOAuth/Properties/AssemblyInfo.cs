78d6ff169b14fcc3c59eb81f4db2456310492957
neuecc <ils@neue.cc>
2013-04-01 21:51:39 +0900

    ready for 0.6.3

--- AsyncOAuth/Properties/AssemblyInfo.cs
+++ AsyncOAuth/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("ja")]
 
-[assembly: AssemblyVersion("0.6.2.0")]
-[assembly: AssemblyFileVersion("0.6.2.0")]
+[assembly: AssemblyVersion("0.6.3.0")]
+[assembly: AssemblyFileVersion("0.6.3.0")]
