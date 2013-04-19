1db86a063112dc943a0c6ea2b22e9288c3ebff80
HATSUNE, Akira <hatsune.a@nifty.com>
2013-04-20 08:24:39 +0900

    Update MainPage.xaml.vb

--- AsyncOAuth.WindowsStoreAppVB/MainPage.xaml.vb
+++ AsyncOAuth.WindowsStoreAppVB/MainPage.xaml.vb
@@ -3,10 +3,10 @@
 Public NotInheritable Class MainPage
     Inherits Page
 
-    Private Const ConsumerKey As String = "Ya69yklLcgAuxbSSft3Dw"
-    Private Const ConsumerSecret As String = "M2EclIKFR3TUwceE59S0ctIXVsrLu8bndSl430A7M"
-    Private Const AccessTokenKey As String = "14565539-IPM7XQisVYKK1xQ5RdOK3j8KRg3eXaISvAE8crceb"
-    Private Const AccessTokenSecret As String = "FOK2eluHpsvnh25z7AnBhWW8zik0Baf8JC9dtfeU8"
+    Private Const ConsumerKey As String = ""
+    Private Const ConsumerSecret As String = ""
+    Private Const AccessTokenKey As String = ""
+    Private Const AccessTokenSecret As String = ""
 
     Private ReadOnly AccessToken As New AccessToken(AccessTokenKey, AccessTokenSecret)
 
